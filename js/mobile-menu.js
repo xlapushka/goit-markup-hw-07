88963e834d3b4c911bc48a33f8366eac91bce2da
Yuliia Hubarenko <xlapushka@ukr.net>
2023-02-22 06:00:34 +0200

    finished (without drying)
    mobile menu (without rotation on 768px)
    modal window
    gaps and etc
    
    //деякі речі прикручені))

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,23 +1,55 @@
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector("[data-menu-open]"),
+//     closeMenuBtn: document.querySelector("[data-menu-close]"),
+//     menu: document.querySelector("[data-menu]"),
+//     body: document.querySelector("body"),
+//     menuList: document.querySelector(".mobile-menu"),
+//   };
+
+//   refs.openMenuBtn.addEventListener("click", toggleMenu);
+//   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+//   refs.menuList.addEventListener("click", removeMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle("is-hidden");
+//     refs.body.classList.toggle("no-scroll");
+//   }
+
+//   // function removeMenu() {
+//   //   refs.menu.classList.toggle("is-hidden");
+//   //   refs.body.classList.toggle("no-scroll");
+//   // }
+// })();
+
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
-    body: document.querySelector("body"),
-    menuList: document.querySelector(".mobile-menu"),
+  const mobileMenu = document.querySelector("[data-menu]");
+  const openMenuBtn = document.querySelector("[data-menu-open]");
+  const closeMenuBtn = document.querySelector("[data-menu-close]");
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    console.log(openMenuBtn);
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    console.log(openMenuBtn);
+    console.log(!isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
+    
+    const scrollLockMethod = !isMenuOpen
+      ? "disableBodyScroll"
+      : "enableBodyScroll";
+    bodyScrollLock[scrollLockMethod](document.body);
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  refs.menuList.addEventListener("click", removeMenu);
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
-  }
-
-  function removeMenu() {
-    refs.menu.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll");
-  }
+  // Close the mobile menu on wider screens if the device orientation changes
+  // window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+  //   if (!e.matches) return;
+  //   mobileMenu.classList.remove("is-open");
+  //   openMenuBtn.setAttribute("aria-expanded", false);
+  //   bodyScrollLock.enableBodyScroll(document.body);
+  // });
 })();
